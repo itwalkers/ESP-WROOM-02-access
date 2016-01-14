3e0e4508fe31d0adf9de78a2174c4d5e93e981f1
WalkerSumida <walkersumida@gmail.com>
2016-01-14 17:49:16 +0900

    定数関係を整理

--- ESP-WROOM-02-access.ino
+++ ESP-WROOM-02-access.ino
@@ -1,11 +1,22 @@
 #include <ESP8266WiFi.h>
 
-// set wifi
-const char* SSID     = "your_ssid";
-const char* PASSWORD = "your_p@$$w0rd";
+/**
+ * constant
+ */
+const char* SSID            = "your_ssid";
+const char* PASSWORD        = "your_p@$$w0rd";
+const char* HOST            = "xxxxx.com";     // set host (xxxxx.com or IP address)
+const char* CONNECTION      = "xxxxx";
+const char* CACHE_CONTROL   = "xxxxx";
+const char* ACCEPT          = "xxxxx";
+const char* UP_INSECURE_REQ = "xxxxx";
+const char* USER_AGENT      = "xxxxx";
+const char* ACCEPT_ENCODING = "xxxxx";
+const char* ACCEPT_LANGUAGE = "xxxxx";
+const int HTTP_PORT         = 80;
+const int ACCESS_INTERVAL   = 5000;            // サーバにアクセスする間隔（あまり短いとサーバに負荷がかかるので注意）
 
-// set host (xxxxx.com or IP address)
-const char* HOST = "xxxxx.com";
+String url = "/set/?val=88";
 
 void setup()
 {
@@ -35,30 +46,28 @@ void setup()
 
 void loop()
 {
-  delay(5000);
+  delay(ACCESS_INTERVAL);
   Serial.print("connecting to ");
   Serial.println(HOST);
 
   WiFiClient client;
-  const int HTTP_PORT = 80;
   if (!client.connect(HOST, HTTP_PORT)) {
     Serial.println("connection failed");
     return;
   }
 
-  String url = "/set/?val=88";
   Serial.print("Requesting URL: ");
   Serial.println(url);
 
   client.print(String("GET ") + url + " HTTP/1.1\r\n" +
                "Host: " + HOST + "\r\n" +
-               "Connection: xxxxx\r\n" + 
-               "Cache-Control: xxxxx\r\n" + 
-               "Accept: xxxxx\r\n" + 
-               "Upgrade-Insecure-Requests: xxxxx\r\n" +
-               "User-Agent: xxxxx\r\n" +
-               "Accept-Encoding: xxxxx\r\n" +
-               "Accept-Language: xxxxx\r\n" +
+               "Connection: " + CONNECTION + "\r\n" + 
+               "Cache-Control: " + CACHE_CONTROL + "\r\n" + 
+               "Accept: " + ACCEPT + "\r\n" + 
+               "Upgrade-Insecure-Requests: " + UP_INSECURE_REQ + "\r\n" +
+               "User-Agent: " + USER_AGENT + "\r\n" +
+               "Accept-Encoding: " + ACCEPT_ENCODING + "\r\n" +
+               "Accept-Language: " + ACCEPT_LANGUAGE + "\r\n" +
                "Connection: close\r\n\r\n");
   delay(2000);
 
